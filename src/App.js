360902463204fae67904b4cab31e00acf3776444
digvijaybind <binddigvijay123@gmail.com>
2023-07-11 00:02:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const url = "https://jsonplaceholder.typicode.com/comments";
+  console.log("data", data);
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("jsonnnn", json);
+        setData(json);
+      })
+      .catch((e) => {
+        console.log("e", e);
+      });
+  }, []);
+  //put event
+  const postEvent = () => {
+    const data = {
+      name: "seeema",
+      mobile: "8788825285",
+      designation: "developer",
+      pin: "454578",
+    };
+    const url = "https://jsonplaceholder.typicode.com/comments";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((json) => setData("jsonnnn", json))
+      .catch((e) => {
+        console.log("e", e);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>welcome</h1>
+      {data?.map((item, index) => {
+        return (
+          <div key={index}>
+            <li>
+              <h6>{item.id}</h6>
+              <p>{item.name}</p>
+              <p>{item.body}</p>
+            </li>
+          </div>
+        );
+      })}
+      <button onClick={postEvent}>submit</button>
     </div>
   );
 }
